482c5f9979086f34108c9d9060518ab4fe382823
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2023-08-07 18:17:45 +0100

    Commit 3
    I added the necessary code for the sign up to work as well (just the frontend )

--- index.js
+++ index.js
@@ -4,30 +4,49 @@ const homeElement = document.querySelector(".home_screen")
 const exploreBtnNav = document.querySelector(".explore")
 const startBtnNav = document.querySelector(".start")
 const homeBtnNav = document.querySelector(".home")
-const exploreElement = document.querySelector(".start_echoing_screen")
+const exploreElementLogin = document.querySelector(".start_echoing_screen_login")
+const exploreElementSignup = document.querySelector(".start_echoing_screen_signup")
+const toSignUpBtn = document.getElementById("to-signup-btn")
+const toLoginBtn = document.getElementById("to-login-btn")
 startBtn.addEventListener("click",function(){
     console.log("start btn ")
-    display_Explore_Screen()
+    display_Explore_Screen_Login()
 })
 exploreBtnNav.addEventListener("click",function(){
     console.log("explorebtn")
     
 })
 startBtnNav.addEventListener("click",function(){
-    display_Explore_Screen()
+    display_Explore_Screen_Login()
 })
 homeBtnNav.addEventListener("click",function(){
     console.log("home page ")
     display_home_screen()
 })
-function display_Explore_Screen(){
+toSignUpBtn.addEventListener("click",function(){
+    display_Explore_Screen_SignUp()
+})
+toLoginBtn.addEventListener("click",function(){
+    display_Explore_Screen_Login()
+})
+
+
+function display_Explore_Screen_SignUp(){
     headerElement.style.display='flex'
     homeElement.style.display='none'
-    exploreElement.style.display='flex'
+    exploreElementLogin.style.display='none'
+    exploreElementSignup.style.display='flex'
+}
+function display_Explore_Screen_Login(){
+    exploreElementSignup.style.display='none'
+    headerElement.style.display='flex'
+    homeElement.style.display='none'
+    exploreElementLogin.style.display='flex'
 }
 function display_home_screen(){
+    exploreElementSignup.style.display='none'
     headerElement.style.display='none'
     homeElement.style.display='flex'
-    exploreElement.style.display='none'
+    exploreElementLogin.style.display='none'
 
 }
