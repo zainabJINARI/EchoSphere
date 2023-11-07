b24fb40785becab6abe7aeedadbd82959a8ea422
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2023-11-07 17:15:11 +0100

    Adding some styling
    Creating the explore echose page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const startBtn = document.getElementById("start-echoing")
-const headerElement = document.querySelector(".header-element")
+const headerElement = document.querySelector("#header-element")
 const homeElement = document.querySelector(".home_screen")
 const exploreBtnNav = document.querySelector(".explore")
 const startBtnNav = document.querySelector(".start")
@@ -8,12 +8,15 @@ const exploreElementLogin = document.querySelector(".start_echoing_screen_login"
 const exploreElementSignup = document.querySelector(".start_echoing_screen_signup")
 const toSignUpBtn = document.getElementById("to-signup-btn")
 const toLoginBtn = document.getElementById("to-login-btn")
+const explore_echoes = document.querySelector('.explore_echoes')
 startBtn.addEventListener("click",function(){
     console.log("start btn ")
     display_Explore_Screen_Login()
 })
 exploreBtnNav.addEventListener("click",function(){
     console.log("explorebtn")
+    display_echos()
+
     
 })
 startBtnNav.addEventListener("click",function(){
@@ -36,17 +39,31 @@ function display_Explore_Screen_SignUp(){
     homeElement.style.display='none'
     exploreElementLogin.style.display='none'
     exploreElementSignup.style.display='flex'
+    explore_echoes.style.display='none'
 }
 function display_Explore_Screen_Login(){
     exploreElementSignup.style.display='none'
     headerElement.style.display='flex'
     homeElement.style.display='none'
     exploreElementLogin.style.display='flex'
+    explore_echoes.style.display='none'
+
 }
 function display_home_screen(){
     exploreElementSignup.style.display='none'
     headerElement.style.display='none'
     homeElement.style.display='flex'
     exploreElementLogin.style.display='none'
+    explore_echoes.style.display='none'
+
+
+}
+function display_echos(){
+    exploreElementSignup.style.display='none'
+    headerElement.style.display='none'
+    homeElement.style.display='none'
+    exploreElementLogin.style.display='none'
+    headerElement.style.display='flex'
+    explore_echoes.style.display='flex'
 
 }
