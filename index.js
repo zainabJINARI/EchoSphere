3cb2d03ec02a830ee7a49eb7a9a10573d0709bec
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2023-11-09 13:39:48 +0100

    Add api to get quotes

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const exploreElementSignup = document.querySelector(".start_echoing_screen_signu
 const toSignUpBtn = document.getElementById("to-signup-btn")
 const toLoginBtn = document.getElementById("to-login-btn")
 const explore_echoes = document.querySelector('.explore_echoes')
+
+
 startBtn.addEventListener("click",function(){
     console.log("start btn ")
     display_Explore_Screen_Login()
@@ -59,11 +61,38 @@ function display_home_screen(){
 
 }
 function display_echos(){
+    
+    //
+ 
+   
+      
+    fetch('https://type.fit/api/quotes')
+    .then(res => res.json())
+    .then(data => {
+        const htmlStrucute = data.map((e)=>{
+            if  ((e.author).split(',')[0]==='type.fit'){ authorName='Unknown'}
+            else{
+                authorName=(e.author).split(',')[0]
+            }
+            return `
+        <div class="echo">
+            <h4>${authorName}</h4>
+            <p class="content">${e.text}</p>
+            <button class="like">
+                <i class="fa-solid fa-heart" style="color: #4d331f;"></i>
+            </button>
+        </div>   
+            `
+        }).join('')
+        explore_echoes.innerHTML=htmlStrucute
+
+    })
     exploreElementSignup.style.display='none'
     headerElement.style.display='none'
     homeElement.style.display='none'
     exploreElementLogin.style.display='none'
     headerElement.style.display='flex'
     explore_echoes.style.display='flex'
+    
 
 }
